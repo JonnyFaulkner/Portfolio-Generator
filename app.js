cd413d5cbe08d77770979a37f17f3fb4bc98713e
Jonathan Faulkner <83264372+JonnyFaulkner@users.noreply.github.com>
2021-07-03 17:09:41 -0500

    added projects section to html

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 const inquirer = require('inquirer')
-// const fs = require('fs')
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', generatePage(name,github), err => {
-//     if (err) throw new Error(err);
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+const fs = require('fs')
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
-return inquirer.prompt([
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
@@ -126,18 +118,24 @@ const promptProject = portfolioData => {
             message: 'Would you like to enter another project?',
             default: false
         }
-        ]).then(projectData => {
-            portfolioData.projects.push(projectData);
-            if (projectData.confirmAddProject) {
-                return promptProject(portfolioData);
-            } else {
-                return portfolioData;
-            }
+    ]).then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
     })
 };
 
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Portfolio complete! Check out index.html to see the output!');
+        });
     });
